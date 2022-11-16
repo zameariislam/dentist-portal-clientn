35083536ddc2ddddb6c25916be8fe3a94bb6cc61
zameariislam <zameariislam@gmail.com>
2022-11-16 20:44:57 +0100

    database connected

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
- const express=require('express')
- const app=express()
- const cors=require('cors')
 
- const port=process.env.PORT||8000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+
+
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 8000
 
 //  middleware 
 
 
- app.use(cors())
- app.use(express.json())
+app.use(cors())
+app.use(express.json())
 
- app.get('/',(req,res)=>{
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vmx3iz0.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri);
+
+
+const dbConnect = async () => {
+
+    try {
+        await client.connect()
+        console.log('database connected')
+
+
+    }
+    catch (error) {
+        console.log(error)
+
+    }
+}
+dbConnect()
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Server is running')
 
- })
+})
 
- 
-  app.listen(port,()=>{
+
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
-  })
+})
 
 
