519735fc8077dcbd9baff0303cc566b1d6b555f3
zameariislam <zameariislam@gmail.com>
2022-11-17 11:32:40 +0100

    limit one booking per treatement per user

--- index.js
+++ index.js
@@ -40,8 +40,112 @@ const dbConnect = async () => {
 }
 dbConnect()
 
+const appoinmentOptionCollection = client.db("doctorsPortal").collection("appoinmentOptions");
+const bookingsCollection = client.db("doctorsPortal").collection("bookings");
 
 
+
+
+
+
+
+app.get('/appoinmentOptions', async (req, res) => {
+
+    const date = req.query.date
+
+    try {
+
+        const query = {}
+
+        const bookingQuery = { appoinmentDate: date }
+
+
+
+
+        const options = await appoinmentOptionCollection.find(query).toArray();
+        const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray()
+        // console.log(alreadyBooked)
+        options.forEach(option => {
+            const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+            const bookedSlot = optionBooked.map(book => book.slot)
+            const remainingSlots = option.slots.filter(slot => !bookedSlot.includes(slot))
+            option.slots = remainingSlots
+
+
+        })
+
+
+        res.send({
+            success: true,
+            message: "Successfully got the data",
+            data: options,
+        });
+    } catch (error) {
+
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
+
+
+})
+
+
+
+app.post('/bookings', async (req, res) => {
+
+
+
+
+    try {
+        const booking = req.body
+        console.log(booking)
+        const query = {
+
+            appoinmentDate: booking.appoinmentDate,
+            treatment: booking.treatment,
+            email: booking.email
+
+        }
+        console.log(query)
+
+
+        const alreadyBooked = await bookingsCollection.find(query).toArray()
+        console.log(alreadyBooked)
+        if (alreadyBooked.length) {
+            return res.send({
+                success: false,
+                message: `You have already have a booking on ${booking.appoinmentDate}`
+            })
+
+
+        }
+
+
+        const result = await bookingsCollection.insertOne(req.body);
+
+
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: `Successfully created  with id ${result.insertedId}`,
+            });
+        } else {
+            res.send({
+                success: false,
+                error: "Couldn't create the product",
+            });
+        }
+    } catch (error) {
+
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
+
+})
 
 
 
